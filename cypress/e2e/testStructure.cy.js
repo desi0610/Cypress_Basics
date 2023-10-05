a6a9666472cc37817424c8c5d4cf1d247a303386
desi0610 <desita_mail@yahoo.com>
2023-10-05 14:54:06 -0400

    add: more test

--- cypress/e2e/testStructure.cy.js
+++ cypress/e2e/testStructure.cy.js
@@ -19,4 +19,20 @@ describe('Context: My First Test',() =>{
         cy.visit('/registration_form');
        
     })
+
+    it('Test 2', () => {
+        expect(false).to.equal(false);
+    })
+
+    it('Test 3', () => {
+        expect(5).to.equal(5);
+    })
+
+    it('Test 4', () => {
+        expect(false).not.to.equal(false);
+    })
+    it('Test 5', () => {
+        expect(true).to.equal('5' == 5);
+    })
+
 })
