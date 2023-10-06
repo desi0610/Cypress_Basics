622cf878e8fca15e9c86a02af0b38571e12451e4
desi0610 <desita_mail@yahoo.com>
2023-10-06 12:32:28 -0400

    add: more tests

---
+++ cypress/e2e/locators.cy.js
@@ -0,0 +1,26 @@
+
+/// <reference types="cypress"/>
+
+describe('Find or get Elements by using Different Locators', () =>{
+    beforeEach(() => {
+        // runs before each test case, beforeMethod at TestNG
+        cy.clearCookies();
+        cy.visit('/login');
+    })
+
+    it('Check different locator strategies', () => {
+        // By CSS locators 
+        cy.get("input[name='username']").type('CydeoStudent'); //every statement creates an obj to be interacted, 
+        // and next command makes operation to the obj created at previous statement
+        // attribute name and value
+
+        cy.get("[type='text']").clear(); //clear what is typed
+
+        cy.get("input").each((item, index, list) => {
+            // assert the lenght of the list is 2
+            expect(list).to.have.length(2);
+            expect(item).to.have.attr("type");
+
+        })
+    })
+})
